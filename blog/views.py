682d9270040b84a63db553476ca2572dde7c64db
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2026-07-07 18:35:41 -0300

    primeiro commit

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    post = Post.objects.all()
+    return render(request,'blog/post_list.html',{'post': post})
