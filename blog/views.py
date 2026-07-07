9f5643534e90ede4211a432e1b4eed072b72d372
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2026-07-07 20:29:58 -0300

    add templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Post
+
+def home(request):
+    return render(request, 'home.html')
+
+
 
 def post_list(request):
     post = Post.objects.all()
-    return render(request,'blog/post_list.html',{'post': post})
+    return render(request, 'blog/post_list.html', {'post': post})
