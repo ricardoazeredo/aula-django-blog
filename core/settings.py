6d2543e6cd1d16ab2c468d62d1fe5201480554e3
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2026-07-07 21:46:10 -0300

    template base criado

--- core/settings.py
+++ core/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']   
+# MEDIA_ROOT = BASE_DIR / 'static/images'
+
+# MEDIA_URL = "imagens/" 
